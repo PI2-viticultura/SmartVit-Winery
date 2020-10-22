02b42d31c940726994a984a86bdaa1e98541a848
Joao Lucas <joao.lucas.ssr@gmail.com>
2020-10-22 20:37:21 -0300

    [feature] adding backend to register sensor

--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,6 @@
 from utils.constants import (
-    DATABASE_CONFIG
+    DATABASE_CONFIG,
+    DATABASE_CONFIG_SENSOR
 )
 import os
 
@@ -21,3 +22,25 @@ def load_database_params():
     }
 
     return params
+
+
+def load_database_sensor_params():
+    port = os.getenv("DB_PORT", DATABASE_CONFIG_SENSOR["DB_PORT"])
+    port = int(port)
+
+    sensor_params = {
+        "host": os.getenv("DB_HOST", DATABASE_CONFIG_SENSOR["DB_HOST"]),
+        "port": port,
+        "username": os.getenv("DB_USERNAME", 
+                              DATABASE_CONFIG_SENSOR["DB_USERNAME"]),
+        "password": os.getenv("DB_PASSWORD", 
+                              DATABASE_CONFIG_SENSOR["DB_PASSWORD"]),
+        "authSource": os.getenv("authSource",
+                              DATABASE_CONFIG_SENSOR["authSource"]),
+        "authMechanism": os.getenv(
+            "authMechanism",
+            DATABASE_CONFIG_SENSOR["authMechanism"]
+        )
+    }
+
+    return sensor_params
