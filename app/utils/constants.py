246885a7b6a9515df2377437cdaaa61a7e489666
Lucas Vitor de Paula <lucasvitorifg@gmail.com>
2020-10-23 21:19:25 -0300

    Revert "US30 - Cadastrar sensor"

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -7,13 +7,3 @@ DATABASE_CONFIG = {
     "authMechanism": "SCRAM-SHA-256",
     "collection": "winery"
 }
-
-DATABASE_CONFIG_SENSOR = {
-    "DB_HOST": "winery-mongodb",
-    "DB_PORT": 27017,
-    "DB_USERNAME": "admin",
-    "DB_PASSWORD": "password",
-    "authSource": "",
-    "authMechanism": "SCRAM-SHA-256",
-    "collection": "sensor"
-}
